60e2c0ff122b85759e5c918d4f298872e06e5f6e
Lisilin87 <94789065+Lisilin87@users.noreply.github.com>
2026-04-08 11:22:09 +0800

    Add voice_test evaluation setup

---
+++ config_ini.py
@@ -0,0 +1,22 @@
+FILE_NAME = "E2E情感陪聊数据集_pre_3.xlsx"
+SHEET_NAME = "Sheet2"
+START_ROW = 2
+
+
+# 前置语音文件，固定参数
+PRE_WAV_PATH = "output_recording.wav"
+# 待读取语音文件所在文件夹
+AUDIO_NAME_COL_PRE = "./voice_input_file/"
+AUDIO_NAME_COL = "音频文件路径"
+# 保存到语音文件所在的文件夹
+SAVE_WAV_PATH = "./voice_output_file_4_2/"
+# 临时存放目录
+SAVE_WAV_TEMP_PATH = "./temp/"
+
+CASE_SON_ID = "用例子编号"
+SAVE_WAV_COL = "回复语音保存路径"
+RESPONSE_COL = "回复的内容"
+EMOTION_COL_1 = "识别到的情绪"
+EMOTION_COL_2 = "回复的情绪"
+
+EXCEL_SAVE_NAME = "./E2E测试结果_第三版"
